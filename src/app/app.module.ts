4e1ee64f0f08a483daa02c654abbcbc8c93323be
Carlos Ortiz <carlos.ortiz@bosonit.com>
2023-08-29 13:46:54 +0200

    adding pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { Html5Component } from './pages/html5/html5.component';
+import { CSS3Component } from './pages/css3/css3.component';
+import { JavascriptComponent } from './pages/javascript/javascript.component';
+import { AngularComponent } from './pages/angular/angular.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Html5Component,
+    CSS3Component,
+    JavascriptComponent,
+    AngularComponent
   ],
   imports: [
     BrowserModule,
